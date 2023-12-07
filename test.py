f6201f1d9f4e9fa760291050ab62cd67ac731867
FionaG26 <fionamuthoni18@gmail.com>
2023-12-07 18:29:33 +0300

    tests

---
+++ test.py
@@ -0,0 +1,23 @@
+import pygame
+
+pygame.init()
+
+# Create a Pygame window
+screen = pygame.display.set_mode((400, 400))
+pygame.display.set_caption("Test Window")
+
+running = True
+while running:
+        for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                                    running = False
+
+                                        # Fill the screen with black color
+                                            screen.fill((0, 0, 0))
+
+                                                # Update the display
+                                                    pygame.display.flip()
+
+                                                    # Quit Pygame
+                                                    pygame.quit()
+
