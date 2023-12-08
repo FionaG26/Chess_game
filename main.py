6ac60897fd94ede5a6c80d79d718498f026652a0
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2023-12-08 10:41:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,4 @@
 import pygame
-import pygame_gui
-from chessboard.chessboard import ChessBoard
-from pieces.piece import ChessPiece
-
-
-def main():
-    pygame.init()
-
-    chess_game = ChessBoard()
-
-    # Example: Create pieces and place them on the chessboard
-    white_pawn = ChessPiece("pawn", "white")
-    black_knight = ChessPiece("knight", "black")
-
-    chess_game.place_piece(1, 2, white_pawn)
-    chess_game.place_piece(6, 3, black_knight)
-
-    chess_game.run_game()
-
-    pygame.quit()
-
-
-if __name__ == "__main__":
-    main()
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Python Chess Game Tutorial with Pygame")
